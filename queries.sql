d0c655caa9810e56f1b03463dd2bf8ec40f896eb
Jhoan Peguero <jpeguero1989@gmail.com>
2020-09-03 18:21:54 -0400

    Done!

--- queries.sql
+++ queries.sql
@@ -1,7 +1,18 @@
 ## Part 1: Test it with SQL
-
+select column_name, data_type
+  from information_schema.columns
+ where table_schema = 'techjobs'
+   and table_name = 'job';
 ## Part 2: Test it with SQL
-
+select *
+from employer
+where location = 'St. Louis City'
 ## Part 3: Test it with SQL
+drop table job
 
 ## Part 4: Test it with SQL
+  select s.name,s.description
+  from skill s
+  inner join job_skills js on js.skills_id = s.id
+  inner join job j on j.id = js.jobs_id
+  order by s.name asc
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,12 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotNull(message = "Name is required")
     private String name;
 
     public int getId() {
